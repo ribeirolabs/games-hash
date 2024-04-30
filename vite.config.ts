5d6e76ca0671b068e01543accfc0932411322991
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2024-04-30 12:13:23 -0300

    fix: base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/hash",
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    base: env.BASE_URL ?? "/",
+  };
 });
